b72e8d8c172c1bc512b8e5259356957f753c05f6
John Yu <john.yu@reisystems.com>
2019-03-13 13:59:40 -0400

    simple description added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ class App extends Component {
           <h1>MineSweeper</h1>
           <Map className="map" />
         </div>
-        <p>This is how you play minesweeper</p>
+        <p>Hpw to play minesweeper:</p>
+        <p>1)The goal of the game is to NOT click any bombs</p>
+        <p>
+          2)Each number tells you how many bombs are adjacent to the " clicked"
+          cell
+        </p>
+        <p>
+          3)The numbers range from 0 to 8. Each cell has 8 neighbors. Count
+          them.
+        </p>
+        <p>4) Use process of elimination to choose where to click next.</p>
       </div>
     );
   }
