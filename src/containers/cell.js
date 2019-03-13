08b6b3fa146aff2838cbd74ef5ed81dde71598aa
John Yu <john.yu@reisystems.com>
2019-03-13 18:39:51 -0400

    end game

--- src/containers/cell.js
+++ src/containers/cell.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 
+let endMineSweeperGame = false;
+
 class Cell extends Component {
   constructor(props) {
     super(props);
@@ -9,16 +11,18 @@ class Cell extends Component {
   handleClick(e) {
     if (!this.state.flag) this.setState({ clicked: true });
     let { row, column } = this.props;
-    if (this.props.value === "") {
+    //recursion cases
+    if (
+      this.props.value === "" &&
+      e.target.id === `${row}_${column}` &&
+      !endMineSweeperGame
+    ) {
       e.target.id = `${row}_${column}_`;
       let rowList = [row - 1, row, row + 1];
       let colList = [column - 1, column, column + 1];
       for (let i of rowList) {
         for (let j of colList) {
-          if (
-            document.getElementById(`${i}_${j}`) &&
-            !(row === i && column === j)
-          ) {
+          if (document.getElementById(`${i}_${j}`)) {
             setImmediate(() => {
               if (document.getElementById(`${i}_${j}`))
                 document.getElementById(`${i}_${j}`).click();
@@ -27,8 +31,24 @@ class Cell extends Component {
         }
       }
     }
+    //end game
+    if (this.props.value === "☀" && !endMineSweeperGame) {
+      endMineSweeperGame = true;
+      e.target.style.backgroundColor = "black";
+      let cols = e.target.parentElement.children.length;
+      let rows = e.target.parentElement.parentElement.children.length;
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+          //   console.log(document.getElementById(`${i}_${j}`));
+          if (document.getElementById(`${i}_${j}`))
+            document.getElementById(`${i}_${j}`).click();
+          if (document.getElementById(`${i}_${j}_`))
+            document.getElementById(`${i}_${j}_`).click();
+        }
+      }
+    }
   }
-  onContextMenu(e) {
+  handleContextMenu(e) {
     e.preventDefault();
     if (!this.state.clicked) {
       if (!this.state.flag) this.setState({ flag: "⚑" });
@@ -47,7 +67,7 @@ class Cell extends Component {
         id={`${row}_${column}`}
         className={cellsClass}
         onClick={this.handleClick.bind(this)}
-        onContextMenu={this.onContextMenu.bind(this)}
+        onContextMenu={this.handleContextMenu.bind(this)}
       >
         {this.state.clicked && !this.state.flag ? this.props.value : ""}
         {this.state.flag}
