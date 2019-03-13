23a32a054fd3ef93754bebd1db65d6a91d9209d0
John Yu <john.yu@reisystems.com>
2019-03-13 17:07:41 -0400

    recursion added

--- src/containers/cell.js
+++ src/containers/cell.js
@@ -5,9 +5,26 @@ class Cell extends Component {
   constructor(props) {
     super(props);
     this.state = { clicked: false };
+    this.recursed = false;
   }
   handleClick() {
     if (!this.state.flag) this.setState({ clicked: true });
+    if (this.props.value === "" && !this.recursed) {
+      this.recursed = true;
+      let { row, column } = this.props;
+      let rowList = [row - 1, row, row + 1];
+      let colList = [column - 1, column, column + 1];
+      for (let i of rowList) {
+        for (let j of colList) {
+          if (
+            document.getElementById(`${i}_${j}`) &&
+            !(row === i && column === j)
+          ) {
+            document.getElementById(`${i}_${j}`).click();
+          }
+        }
+      }
+    }
   }
   onContextMenu(e) {
     e.preventDefault();
@@ -17,12 +34,14 @@ class Cell extends Component {
     }
   }
   render() {
+    let { row, column } = this.props;
     let cellsClass = classNames({
       cell: true,
       clicked: this.state.clicked
     });
     return (
       <td
+        id={`${row}_${column}`}
         className={cellsClass}
         onClick={this.handleClick.bind(this)}
         onContextMenu={this.onContextMenu.bind(this)}
