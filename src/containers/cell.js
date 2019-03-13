8005ebce72dcc55d44d4fe20391558a3e050db95
John Yu <john.yu@reisystems.com>
2019-03-13 13:55:46 -0400

    flag and bomb added as symbols, logic added

--- src/containers/cell.js
+++ src/containers/cell.js
@@ -9,49 +9,29 @@ class Cell extends Component {
     super(props);
     this.state = { clicked: false };
   }
-  componentDidMount() {
-    // console.log(this.props);
-    // let { value } = this.props;
-  }
   handleClick() {
-    this.setState({ clicked: false });
+    if (!this.state.flag) this.setState({ clicked: true });
   }
-  onContextMenu(e) {}
+  onContextMenu(e) {
+    e.preventDefault();
+    if (!this.state.clicked) {
+      if (!this.state.flag) this.setState({ flag: "⚑" });
+      else this.setState({ flag: "" });
+    }
+  }
   render() {
     let cellsClass = classNames({
-      cell: true
+      cell: true,
+      clicked: this.state.clicked
     });
-    if (this.props.image !== undefined) {
-      const images = "./src/static/images";
-      let map_images = {
-        0: "number0",
-        1: "number1",
-        2: "number2",
-        3: "number3",
-        4: "number4",
-        5: "number5",
-        6: "number6",
-        7: "number7",
-        8: "number8",
-        9: "flag",
-        10: "bomb"
-      };
-      let top = 50 + this.props.row * 25 + "px";
-      let left = 500 + 25 * this.props.column + "px";
-      var divStyle = {
-        // backgroundImage: `url(${images}/${map_images[this.props.image]}.png)`,
-        top,
-        left
-      };
-    }
     return (
       <td
         className={cellsClass}
-        style={divStyle}
         onClick={this.handleClick.bind(this)}
         onContextMenu={this.onContextMenu.bind(this)}
       >
-        {this.state.clicked ? this.props.value : ""}
+        {this.state.clicked && !this.state.flag ? this.props.value : ""}
+        {this.state.flag}
       </td>
     );
   }
--- src/reducers/reducer_map.js
+++ src/reducers/reducer_map.js
@@ -3,8 +3,8 @@ import { LEFT_CLICK, RIGHT_CLICK } from "../actions";
 import { nestedArray } from "../helpers";
 
 let initialState = valsAdjacentCounts(
-  populateNestedArray(nestedArray(10, 10), 9, 10),
-  9
+  populateNestedArray(nestedArray(10, 10), "☀", 10),
+  "☀"
 );
 
 export default function(state = initialState, action) {
@@ -28,7 +28,7 @@ function valsAdjacentCounts(nestedArray, val) {
   for (let i = 0; i < nestedArray.length; i++) {
     for (let j = 0; j < nestedArray[0].length; j++) {
       if (nestedArray[i][j] === val) {
-        nestedArray = addOneNestedArrAdjacents(nestedArray, i, j);
+        nestedArray = addOneNestedArrAdjacents(nestedArray, i, j, val);
       }
     }
   }
@@ -39,32 +39,19 @@ function floorRand(scale) {
   return Math.floor(Math.random() * scale);
 }
 
-function addOneNestedArrAdjacents(nestedArray, i, j) {
+function addOneNestedArrAdjacents(nestedArray, i, j, val) {
   let iList = [i - 1, i, i + 1];
   let jList = [j - 1, j, j + 1];
   for (let a of iList) {
     for (let b of jList) {
       if (nestedArray[a]) {
         if (nestedArray[a][b] !== undefined) {
-          if (typeof nestedArray[a][b] != "number") nestedArray[a][b] = 0;
-          if (nestedArray[a][b] !== 9) nestedArray[a][b]++;
+          if (typeof nestedArray[a][b] != "number" && nestedArray[a][b] !== val)
+            nestedArray[a][b] = 0;
+          if (nestedArray[a][b] !== val) nestedArray[a][b]++;
         }
       }
     }
   }
   return nestedArray;
 }
-// displays
-9; //bomb
-0; //is empty string
-1;
-2;
-3;
-4;
-5;
-6;
-7;
-8;
-// actuals
-10 - 19;
-// recursed --> // ??probability
